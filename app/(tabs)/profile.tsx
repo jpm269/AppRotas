59d1e7a93dce5c2dc53b72acee8955df36cd4f52
Joao Martins <joaomartinssabugo@gmail.com>
2026-03-07 18:25:37 +0000

    profile page preenchida

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,51 +1,86 @@
 import { Ionicons } from "@expo/vector-icons";
 import React from "react";
-import { StyleSheet, TextInput, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-export default function profile() {
-    return(
-        <View style={styles.container}>
-              <View style={styles.searchContainer}>
-                <Ionicons name="search" size={20} color="#666" />
-        
-                <TextInput
-                  placeholder="Pesquisar..."
-                  placeholderTextColor="#999"
-                  style={styles.input}
-                />
-              </View>
+const options = [
+  { id: 1, title: "Notificações", icon: "notifications-outline" },
+  { id: 2, title: "Subscrição", icon: "card-outline" },
+  { id: 3, title: "Idioma", icon: "language-outline" },
+  { id: 4, title: "Termos e Condições", icon: "document-text-outline" },
+  { id: 5, title: "Pagamento", icon: "wallet-outline" },
+  { id: 6, title: "Histórico", icon: "time-outline" },
+  { id: 7, title: "Permissões", icon: "shield-checkmark-outline" },
+  { id: 8, title: "Ajuda", icon: "help-circle-outline" },
+  { id: 9, title: "Terminar Sessão", icon: "close-circle-outline" },
+];
+
+export default function Profile() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleCard}>
+        <Text style={styles.title}>Perfil</Text>
+      </View>
+
+      <View>
+        {options.map((item) => (
+          <TouchableOpacity key={item.id} style={styles.option}>
+            <View style={styles.leftContent}>
+              <Ionicons name={item.icon as any} size={30} color="#2B1A12" />
+              <Text style={styles.optionText}>{item.title}</Text>
             </View>
-    );
+
+            {/* <Ionicons name="chevron-forward" size={20} color="#7A6A5F" /> */}
+          </TouchableOpacity>
+        ))}
+      </View>
+
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#F5F0E8",
-    padding: 20,
+    padding: 30,
+    paddingTop: 100
   },
-  map: {
-    flex: 1
+
+  titleCard: {
+    backgroundColor: "#9D500C",
+    borderRadius: 8,
+    width: 143.5,
+    height: 34,
+    borderWidth: 2,
+    borderColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+    marginBottom: 30,
   },
+
   title: {
-    fontSize: 28,
+    fontSize: 24,
     fontWeight: "bold",
-    marginBottom: 20,
+    color: "#fff",
   },
-  searchContainer: {
+
+  option: {
     flexDirection: "row",
     alignItems: "center",
-    backgroundColor: "#fff",
-    borderRadius: 12,
-    paddingHorizontal: 12,
-    height: 50,
-    marginTop: 52,
-    borderWidth: 1,
-    borderColor: "#ddd",
+    justifyContent: "space-between",
+    paddingVertical: 12,
   },
-  input: {
-    flex: 1,
-    marginLeft: 8,
-    fontSize: 16,
+
+  leftContent: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+
+  optionText: {
+    fontSize: 17,
+    color: "#9D500C",
+    fontWeight: "800",
   },
 });
